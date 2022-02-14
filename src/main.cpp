e76ba40b3bd2109893d398547cccadb64e8c7fc1
datawater <codingwithdatagogiberidze@gmail.com>
2022-02-14 20:33:51 +0400

    Added the ability to clear the database, added a settings menu and ascii art for all of the menus

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include <sstream>
 
 #include <unistd.h>
-#include <assert.h>
+
+bool fast_print = false;
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -170,7 +171,7 @@ void todo(char* msg) {
 	printf("TODO: ");
 	printf("%s\n", msg);
 	console(RESET);
-	assert(1);
+	//exit(1);
 }
 
 bool file_exists(char* fname) {
@@ -190,6 +191,7 @@ bool valid_books_file(std::string fname) {
 	fgets(line, sizeof(line), fp);
 	//check if it is a valid header
 	if (strcmp(line, "CALM_LIBRARY\n") != 0) {
+		fclose(fp);
 		return false;
 	}
 	fclose(fp);
@@ -197,6 +199,98 @@ bool valid_books_file(std::string fname) {
 	return true;
 }
 
+bool valid_settings_file(std::string fname) {
+	FILE *fp = fopen(fname.c_str(), "r");
+	if (fp == NULL) {
+		return false;
+	}
+	//read the first line
+	char line[256];
+	fgets(line, sizeof(line), fp);
+	if (strcmp(line, "CALM_SETTINGS\n") != 0) {
+		fclose(fp);
+		return false;
+	}
+	fclose(fp);
+	return true;
+}
+
+void ascii_hello() {
+	//ascii art by http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+
+	printf(" _     _  \n");
+	printf("| |   (_) \n");
+	printf("| |__  _  \n");
+	printf("| '_ \\| | \n");
+	printf("| | | | | \n");
+	printf("|_| |_|_| \n");
+
+	for (int i = 0; i < 4; ++i) {
+		printf("\n");
+	}
+}
+
+void ascii_bye() {
+	//ascii art by http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+	
+	printf(" _\n");
+	printf("| |\n");
+	printf("| |__  _   _  ___ \n");
+	printf("| '_ \\| | | |/ _ \\\n");	
+	printf("| |_) | |_| |  __/\n");
+	printf("|_.__/ \\__, |\\___|\n");
+	printf("        __/ |      \n");
+	printf("       |___/       \n");
+	
+	for (int i = 0; i < 4; ++i) {
+		printf("\n");
+	}
+}
+
+void ascii_search() {
+	//ascii art by http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+
+	printf("                         _     \n");
+	printf("                     \t| |    \n");
+	printf(" ___  ___  __ _ _ __ ___| |__  \n");
+	printf("/ __|/ _ \\/ _` | '__/ __| '_ \\ \n");
+	printf("\\__ \\  __/ (_| | | | (__| | | |\n");
+	printf("|___/\\___|\\__,_|_|  \\___|_| |_|");
+
+	for (int i = 0; i < 4; ++i) {
+		printf("\n");
+	}
+
+}
+
+void ascii_add() {
+	//ascii art by http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+	
+	printf("           _     _ \n");
+	printf("          | |   | |\n");
+	printf("  __ _  __| | __| |\n");
+	printf(" / _` |/ _` |/ _` |\n");
+	printf("| (_| | (_| | (_| |\n");
+	printf(" \\__,_|\\__,_|\\__,_|\n");
+
+	for (int i = 0; i < 4; ++i) {
+		printf("\n");
+	}
+}
+
+void ascii_clear() {
+	printf("       _                 \n      | |                \n   ___| | ___  __ _ _ __ \n  / __| |/ _ \\/ _` | '__|\n | (__| |  __/ (_| | |   \n  \\___|_|\\___|\\__,_|_|   \n                         \n                         ");
+	for (int i = 0; i < 4; ++i) {
+		printf("\n");
+	}
+}
+
+void ascii_settings(){
+	printf("           _   _   _                 \n          | | | | (_)                \n  ___  ___| |_| |_ _ _ __   __ _ ___ \n / __|/ _ \\ __| __| | '_ \\ / _` / __|\n \\__ \\  __/ |_| |_| | | | | (_| \\__ \\\n |___/\\___|\\__|\\__|_|_| |_|\\__, |___/\n                            __/ |    \n                           |___/     ");
+	for (int i = 0; i < 4; ++i) {
+		printf("\n");
+	}
+}
 
 std::string uinput() {
 	std::string result;
@@ -212,6 +306,10 @@ std::string uinput() {
 }
 
 void add_book() {
+
+	console(CLEAR);
+	ascii_add();
+
 	//ask for the database file
 	std::string db_file;
 	console(GREEN);
@@ -333,6 +431,10 @@ void add_book() {
 }
 
 void search_book() {
+
+	console(CLEAR);
+	ascii_search();
+
 	std::string db_file;
 	console(GREEN);
 	printf("Enter the database file: ");
@@ -352,14 +454,14 @@ void search_book() {
 		console(RED);
 		printf("database file Doesn't exists\n");
 		console(RESET);
-		assert(1);
+		exit(1);
 	}
 
 	if (!valid_books_file(db_file)) {
 		console(RED);
 		printf("Invalid database file\n");
 		console(RESET);
-		assert(1);
+		exit(1);
 	}
 
 	console(GREEN);
@@ -425,6 +527,7 @@ void search_book() {
 			while (std::getline(ss2, line2)) {
 				printf("%s\n", line2.c_str());
 			}
+		
 			console(RESET);
 		}
 	} else if (c == *"2") {
@@ -437,46 +540,169 @@ void search_book() {
 	}
 }
 
-//TODO: Implement more functions
+void clear_db() {
+
+	console(CLEAR);
+	ascii_clear();
+
+	console(RED);
+	printf("Are you sure that you want to erase the whole database? [y/n]\n");
+	console(RESET);
+
+	fflush(stdin);
+	std::string choice = uinput();
+
+	//printf(choice.c_str());
+
+	if (choice == "n" || choice == "N") {
+		printf("Quiting");
+		exit(0);
+	}
+
+	std::string db_file;
+	console(GREEN);
+	printf("Enter the database file: ");
+	console(RESET);
+	fflush(stdin);
+	db_file = uinput();
+
+	printf("\n");
+
+	if (db_file.find(".calm") == std::string::npos) {
+		db_file += ".calm";
+	}
+	sleep(0.2);
+
+	if (!file_exists(&db_file[0])) {
+		console(RED);
+		printf("database file Doesn't exists\n");
+		console(RESET);
+		exit(1);
+	}
+	if (!valid_books_file(db_file)) {
+		console(RED);
+		printf("Invalid database file\n");
+		console(RESET);
+		exit(1);
+	}
+
+	std::ofstream _ofs;
+	_ofs.open(db_file, std::ofstream::out | std::ofstream::trunc);
+	_ofs << "CALM_LIBRARY\n";
+	_ofs.close();
+
+	console(GREEN);
+	printf("Succesfully cleared the Database file");
+
+}
+
+void settings_init() {
+	//check if the file exists
+	if (!file_exists((char *)".calm_settings")) {
+		//create the file
+		std::ofstream _ofs;
+		_ofs.open(".calm_settings", std::ofstream::out);
+		_ofs << "CALM_SETTINGS\n";
+		_ofs << "fast_print: false\n";
+	} else {
+		if (!valid_settings_file(".calm_settings")) {
+			console(RED);
+			printf("Invalid Or settings file Do you want to Recover it [y/n]\n");
+			console(RESET);
+			fflush(stdin);
+			std::string choice = uinput();
+			if (choice == "y" || choice == "Y") {
+				std::ofstream _ofs;
+				_ofs.open(".calm_settings", std::ofstream::out | std::ofstream::trunc);
+				_ofs << "CALM_SETTINGS\n";
+				_ofs << "fast_print: false\n";
+				_ofs.close();
+				console(GREEN);
+				printf("Succesfully recovered the settings file\n");
+				console(RESET);
+			} else {
+				exit(1);
+			}
+		}
+
+		std::ifstream _fi(".calm_settings", std::ios::binary);
+		//read the file line by line
+		std::string line;
+		while (std::getline(_fi, line)) {
+			if (line.find("fast_print") != std::string::npos) {
+				if (line.find("true") != std::string::npos) {
+					fast_print = true;
+				} else {
+					fast_print = false;
+				}
+			}
+		}
+	}
+}
+
+void settings_menu() {
+	console(CLEAR);
+	ascii_settings();
+
+	console(GREEN);
+	printf("[1] ");
+	console(RESET);
+	console (YELLOW);
+	printf("Fast Print (Not print some text charachter by charachter) current setting: ");
+	if (fast_print) {
+		printf("true\n");
+	} else {
+		printf("false\n");
+	}
+
+	fflush(stdin);
+	char c = getchar();
+	if (c == *"1") {
+		//change the setting
+		if (fast_print) {
+			fast_print = false;
+		} else {
+			fast_print = true;
+		} 
+		std::ofstream _ofs;
+		_ofs.open(".calm_settings", std::ofstream::out | std::ofstream::trunc);
+		_ofs << "CALM_SETTINGS\n";
+		_ofs << "fast_print: ";
+		if (fast_print) {
+			_ofs << "true\n";
+		} else {
+			_ofs << "false\n";
+		}
+		_ofs.close();
+		console(GREEN);
+		printf("Succesfully changed the setting to: ");
+		console(RESET);
+		console(YELLOW);
+		if (fast_print) {
+			printf("true\n");
+		} else {
+			printf("false\n");
+		}
+		console(RESET);
+	} else if (c == *"\n") {} else {
+		console(RED);
+		printf("Invalid option\n");
+		console(RESET);
+		exit(1);
+	}
+
+	console(RESET);
+}
 
 void charbychar(char *msg, int delay) {
 	for (size_t i = 0; i < strlen(msg); ++i) {
 		printf("%c", msg[i]);
-		sleep(delay);
+		if (!fast_print) {
+			sleep(delay);
+		}
 	}
 }
 
-void ascii_hello() {
-	//ascii art by http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
-
-	printf(" _     _  \n");
-	printf("| |   (_) \n");
-	printf("| |__  _  \n");
-	printf("| '_ \\| | \n");
-	printf("| | | | | \n");
-	printf("|_| |_|_| \n");
-
-	for (int i = 0; i < 4; ++i) {
-		printf("\n");
-	}
-}
-
-void ascii_bye() {
-	//ascii art by http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
-	
-	printf(" _\n");
-	printf("| |\n");
-	printf("| |__  _   _  ___ \n");
-	printf("| '_ \\| | | |/ _ \\\n");	
-	printf("| |_) | |_| |  __/\n");
-	printf("|_.__/ \\__, |\\___|\n");
-	printf("        __/ |      \n");
-	printf("       |___/       \n");
-	
-	for (int i = 0; i < 4; ++i) {
-		printf("\n");
-	}
-}
 
 void welcome() {
 	ascii_hello();
@@ -499,6 +725,18 @@ void usage() {
 	printf("Search For A book\n");
 	console (RESET);
 	console(GREEN);
+	printf("[3] ");
+	console(RESET);
+	console (YELLOW);
+	printf("Clear the database\n");
+	console(RESET);
+	console(GREEN);
+	printf("[s] ");
+	console(RESET);
+	console(YELLOW);
+	printf("Settings\n");
+	console(RESET);
+	console(GREEN);
 	printf("[q] ");
 	console(RESET);
 	console (YELLOW);
@@ -518,21 +756,26 @@ void bye() {
 void what_do() {
 	char c = getchar();
 
-	if (c == *"1") {
-		add_book();
-	} else if (c == *"2") {
-		search_book();
-	} else if (c == *"\n") {assert(1);}
+	if (c == *"1") {add_book();} 
+	else if (c == *"2") {search_book();} 
+	else if (c == *"3") {clear_db();}
 	else if (c == *"q") {bye();}
+	else if (c == *"s") {settings_menu();}
+	else if (c == *"\n") {exit(0);}
 	else {
 		printf("Invalid input\n");
-		assert(1);
+		exit(1);
 	}
 }
 
 bool first_join = true;
 
-int main() {
+int main(int argc, char *argv[]) {
+
+	(void) argc;
+	(void) argv;
+
+	settings_init();
 
 	console(CLEAR);
 
